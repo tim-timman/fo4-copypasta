34ac7b2053a83017807bb9003e6266d9723c5d88
tim-timman <tim.den.vilde@gmail.com>
2017-03-15 19:54:01 +0100

    More general batch files

--- FetchFromFO4Dir.bat
+++ FetchFromFO4Dir.bat
@@ -1,11 +1,24 @@
 @echo off
 echo Fetches any newer Copypasta related files from the Data directory
-echo 
-set dataPath=%ProgramFiles(x86)%\Steam\steamapps\common\Fallout 4\Data
-set gitPath=%~dp0\Data
-xcopy "%dataPath%\meshes\Copypasta" "%gitPath%\meshes\Copypasta" /DYS
-xcopy "%dataPath%\Textures\Copypasta" "%gitPath%\Textures\Copypasta" /DYS
-xcopy "%dataPath%\Scripts\Copypasta_v2" "%gitPath%\Scripts\Copypasta_v2" /DYS
-xcopy "%dataPath%\Scripts\Source\User\Copypasta_v2" "%gitPath%\Scripts\Source\User\Copypasta_v2" /DYS
-xcopy "%dataPath%\Copypasta.esp" "%gitPath%\Copypasta.esp" /DY
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME=HKLM\Software\WOW6432Node\Bethesda Softworks\Fallout4
+set VALUE_NAME=Installed Path
+set gitPath=%~dp0Data
+
+for /F "usebackq tokens=3*" %%A IN (`reg query "%KEY_NAME%" /v "%VALUE_NAME%" 2^>nul ^| find "%VALUE_NAME%"`) do (
+	set dataPath=%%B\Data
+)
+
+if defined dataPath (
+	@echo off
+	xcopy "%dataPath%\meshes\Copypasta" "%gitPath%\meshes\Copypasta" /DYS
+	xcopy "%dataPath%\Textures\Copypasta" "%gitPath%\Textures\Copypasta" /DYS
+	xcopy "%dataPath%\Scripts\Copypasta_v2" "%gitPath%\Scripts\Copypasta_v2" /DYS
+	xcopy "%dataPath%\Scripts\Source\User\Copypasta_v2" "%gitPath%\Scripts\Source\User\Copypasta_v2" /DYS
+	xcopy "%dataPath%\Copypasta.esp" "%gitPath%\Copypasta.esp" /DY
+) else (
+	@echo off
+	echo Installation Path not found.
+)
 pause
--- PushToFO4Dir.bat
+++ PushToFO4Dir.bat
@@ -1,7 +1,20 @@
 @echo off
-echo Pushes the Copypasta files to the Data directory
-echo 
-set dataPath=%ProgramFiles(x86)%\Steam\steamapps\common\Fallout 4\Data
-set gitPath=%~dp0\Data
-xcopy "%gitPath%" "%dataPath%" /D /-Y /S
+echo Pushes any files in the Copypasta local repository to the Data directory
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME=HKLM\Software\WOW6432Node\Bethesda Softworks\Fallout4
+set VALUE_NAME=Installed Path
+set gitPath=%~dp0Data
+
+for /F "usebackq tokens=3*" %%A IN (`reg query "%KEY_NAME%" /v "%VALUE_NAME%" 2^>nul ^| find "%VALUE_NAME%"`) do (
+	set dataPath=%%B\Data
+)
+
+if defined dataPath (
+	@echo off
+	xcopy "%gitPath%" "%dataPath%" /D /-Y /S
+) else (
+	@echo off
+	echo Installation Path not found.
+)
 pause
